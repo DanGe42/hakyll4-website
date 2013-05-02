154f7775161a789369a7ef6b3f40b0b735d44a83
Daniel Ge <dange@seas.upenn.edu>
2013-05-02 16:44:03 -0400

    Add blog engine code

--- site.hs
+++ site.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Hakyll
+import Data.Monoid (mappend)
+import System.FilePath (dropExtension, (</>))
 
 main :: IO ()
 main = hakyll $ do
@@ -27,5 +29,46 @@ main = hakyll $ do
       >>= loadAndApplyTemplate "templates/default.html" defaultContext
       >>= relativizeUrls
 
+  match "blog/*" $ do
+    route   $ setExtension "html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/post.html"    postCtx
+      >>= loadAndApplyTemplate "templates/default.html" postCtx
+      >>= relativizeUrls
+
+  --match "blog.html" $ route routeToDir
+  create ["blog.html"] $ do
+    route idRoute
+    compile $ do
+      let blogCtx =
+            field "posts" (\_ -> postList recentFirst) `mappend`
+            constField "title" "Archive"               `mappend`
+            defaultContext
+
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/blog_index.html" blogCtx
+        >>= loadAndApplyTemplate "templates/default.html"  blogCtx
+        >>= relativizeUrls
+
   match "templates/*" $
     compile templateCompiler
+
+
+-- Utility functions -----------------------------------------------------------
+
+-- Blatantly stolen from example Hakyll code
+postCtx :: Context String
+postCtx = dateField "date" "%B %e, %Y" `mappend` defaultContext
+
+-- Also blatantly ripped from example code
+postList :: ([Item String] -> Compiler [Item String]) -> Compiler String
+postList sortFilter = do
+  posts   <- sortFilter =<< loadAll "blog/*"
+  itemTpl <- loadBody "templates/post_item.html"
+  list    <- applyTemplateList itemTpl postCtx posts
+  return list
+
+routeToDir :: Routes
+routeToDir = customRoute fileToDir
+  where
+    fileToDir x = dropExtension (toFilePath x) </> "index.html"
